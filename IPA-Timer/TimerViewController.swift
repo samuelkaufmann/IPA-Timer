d0bcc0cff3a1c03338931766158eaffe5031c443
Samuel Kaufmann <samuel@smartfactory.ch>
2018-06-15 14:24:12 +0200

    creating timer-view

---
+++ IPA-Timer/TimerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimerViewController.swift
+//  IPA-Timer
+//
+//  Created by Samuel Kaufmann on 15.06.18.
+//  Copyright © 2018 Samuel Kaufmann. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
